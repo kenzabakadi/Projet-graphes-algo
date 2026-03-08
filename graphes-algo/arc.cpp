f8cd2276c15c2c8117d59285013545ae9f064b61
kenzabakadi <kenzabakadi@gmail.com>
2026-03-08 11:08:54 +0100

    Ajout de la classe Arc

---
+++ graphes-algo/arc.cpp
@@ -0,0 +1,33 @@
+#include "arc.h"
+
+Arc::Arc() : poids(0) {}
+
+Arc::Arc(Sommet depart, Sommet arrivee, int poids) {
+    this->depart = depart;
+    this->arrivee = arrivee;
+    this->poids = poids;
+}
+
+void Arc::attribuerPoids(int poids) {
+    this->poids = poids;
+}
+
+void Arc::attribuerSommetDepart(Sommet depart) {
+    this->depart = depart;
+}
+
+void Arc::attribuerSommetArrivee(Sommet arrivee) {
+    this->arrivee = arrivee;
+}
+
+int Arc::retournerPoids() {
+    return poids;
+}
+
+Sommet Arc::retournerSommetDepart() {
+    return depart;
+}
+
+Sommet Arc::retournerSommetArrivee() {
+    return arrivee;
+}
---
+++ graphes-algo/arc.h
@@ -0,0 +1,25 @@
+#ifndef ARC_H
+#define ARC_H
+
+#include "sommet.h"
+
+class Arc {
+private :
+    int poids;
+    Sommet depart;
+    Sommet arrivee;
+
+public:
+    Arc();
+    Arc(Sommet depart, Sommet arrivee, int poids);
+
+    void attribuerPoids(int poids);
+    void attribuerSommetDepart(Sommet depart);
+    void attribuerSommetArrivee(Sommet arrivee);
+
+    int retournerPoids();
+    Sommet retournerSommetDepart();
+    Sommet retournerSommetArrivee();
+};
+
+#endif // ARC_H
