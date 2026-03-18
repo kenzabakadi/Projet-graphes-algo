130eb80d826ab39ba201a3d92c0aabebd921c8ee
sten <t.vuffray@gmail.com>
2026-03-18 17:18:21 +0100

    Ajout : avancé sur Dijkstra mais non terminé

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -2,6 +2,37 @@
 
 Dijkstra::Dijkstra(Graphe* graphe) : Algorithme(graphe) {}
 
+void Dijkstra::verifierPoids() const {
+    for (const Arc& a : g->getArcs()) {
+        if (a.retournerPoids() < 0) {
+            throw std::logic_error("Dijkstra impossible : poids négatif");
+        }
+    }
+}
+
+void Dijkstra::recupererFsAps(vector<int>& fs, vector<int>& aps) const {
+    g->calculerFsAps(fs, aps);
+}
+
+vector<vector<int>> Dijkstra::construireMatriceCout(int n) const {
+    const int MAXPOIDS = 100;
+    vector<vector<int>> c(n+1, vector<int>(n+1, MAXPOIDS));
+
+    for (int i = 1; i <= n; ++i) {
+        c[i][i] = 0;
+    }
+
+    for (const Arc& a : g->getArcs()) {
+        int u = a.retournerSommetDepart().retournerId();
+        int v = a.retournerSommetArrivee().retournerId();
+        c[u][v] = a.retournerPoids();
+    }
+
+    return c;
+}
+
 void Dijkstra::executer() {
-    
+    verifierPoids();
+    matriceCout = construireMatriceCout();
+    dijkstra();
 }
--- dijkstra.h
+++ dijkstra.h
@@ -4,6 +4,17 @@
 #include "algorithme.h"
 
 class Dijkstra : public Algorithme {
+    private: 
+        vector<int> fs;
+        vector<int> aps;
+        vector<int> distances;
+        vector<int> predecesseur;
+        vector<vector<int>> matriceCout;
+
+        void verifierPoids() const;
+        void recupererFsAps(vector<int>& fs, vector<int>& aps) const;
+        vector<vector<int>> construireMatriceCout(int n) const;
+        void dijkstra(int s, const vector<int>& fs, const vector<int>& aps, const vector<vector<int>> c, vector<int>& d)
     public: 
         Dijkstra(Graphe* graphe);
         void executer() override;
