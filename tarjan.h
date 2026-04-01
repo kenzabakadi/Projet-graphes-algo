6ed6838b30e8e4f1598265f358ffd8e95092ffec
sten <t.vuffray@gmail.com>
2026-04-01 17:27:58 +0200

    Ajout : implémentation de la classe Tarjan avec les différentes méthodes associées

--- tarjan.h
+++ tarjan.h
@@ -5,10 +5,31 @@
 
 class Tarjan : public Algorithme {
     private: 
+        int compteur; 
+        int nbCFC;
         vector<int> fs;
         vector<int> aps;
+
+        vector<int> num; // num[sommet] -> ordre de visite du DFS 
+        vector<int> ro; // ro[sommet] -> plus petit num atteignable depuis s 
+        vector<int> pred;
+
+        vector<int> prem;
+        vector<int> pilch;
+        vector<int> cfc; 
+
+        vector<int> tarj;
+        vector<bool> dansTarj;  
+
+        vector<int> fsReduit;
+        vector<int> apsReduit;
+        vector<int> base;
+
+        void traversee(int s);
+        void construireGrapheReduit();
+        void calculerBases(); // il faut encore afficher les bases ! 
     public: 
-        Tarjan(Graphe* graphe);
+        Tarjan(Graphe& graphe);
         void executer() override;
 };
 
